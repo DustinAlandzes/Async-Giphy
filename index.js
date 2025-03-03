7b6029f72a04008aa98b95e3b9f8effc63f18e1d
Dustin Alandzes <5882512+DustinAlandzes@users.noreply.github.com>
2025-03-03 12:24:18 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ console.log(process.env.API_KEY)
 const query = "pachinko"
 const base_url = "https://api.giphy.com/v1/gifs/search"
 
-console.log(endpoint)
 
 async function getImage(query){
     const endpoint = `${base_url}?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=r&lang=en&bundle=messaging_non_clips`
