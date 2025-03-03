020095d4f69ae0f8d6f15cf33b7a3b428ce3029b
PhongT06 <163555884+PhongT06@users.noreply.github.com>
2025-03-03 12:21:46 -0600

    Update index.js
    Co-authored-by: Dustin Alandzes <5882512+DustinAlandzes@users.noreply.github.com>

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ const base_url = "https://api.giphy.com/v1/gifs/search"
 console.log(endpoint)
 
 async function getImage(query){
+    const endpoint = `${base_url}?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=r&lang=en&bundle=messaging_non_clips`
     const response = await fetch(endpoint);
     const data = await response.json();
-    return (data.data[0].url)
+    return data.data[0].url
 }
-getImage('query')
+
+console.log(getImage('query'))
